e9f1625fe6a9eea3688423f323f896e432f20c3c
halac <halac122@gmail.com>
2021-09-13 13:39:11 +0300

    Added Some Features

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
     const posts = ref([])
@@ -6,11 +7,15 @@ const getPosts = () => {
 
       const load = async () => {
         try {
-          let data = await fetch('http://localhost:3000/posts')
-          if(!data.ok){
-            throw Error('no data Available')
-          }
-          posts.value = await data.json()
+          const res = await projectFirestore.collection('posts').get()
+          // console.log(res)
+
+          posts.value = res.docs.map(doc => {
+            // console.log(doc.data())
+            return { ...doc.data(), id: doc.id }
+          })
+    
+
         } catch (err) {
           error.value = err.message
           console.log(error.value)
---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBGo60TBeYploqzlHFgIclwCFPqVtoEH48",
+    authDomain: "abdirisak-firebase-site.firebaseapp.com",
+    projectId: "abdirisak-firebase-site",
+    storageBucket: "abdirisak-firebase-site.appspot.com",
+    messagingSenderId: "428907351709",
+    appId: "1:428907351709:web:fda1734a7aeb1360d78e74"
+  };
+  
+  // init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  // init firestore service
+  const projectFirestore = firebase.firestore()
+
+  // export firestore
+  export { projectFirestore }
